2f12ee90968192d8038eff8e201a0afeb0022157
於伟(融汇/平台研发中心) <wei.yu@yintech.cn>
2020-08-10 15:03:11 +0800

    test git

---
+++ test_git.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''=================================================
+@Project -> File   ：file_filter -> test_git
+@IDE    ：PyCharm
+@Author ：Mr. Yu
+@Date   ：2020/8/10 3:02 下午
+@Desc   ：
+=================================================='''
+import time
+
+print(time.time())
